5a441cdbe17e79048cf2d0a81a6168df7ad99f5c
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-05 23:12:50 +0200

    cleaned the app module by removing controller and service and added port from env in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const portEnv = process.env.PORT;
+  const port = portEnv ? Number(portEnv) : 3000;
+
+  await app.listen(port);
+  console.log(' room reservation system is running on port ' + port);
 }
 bootstrap();
