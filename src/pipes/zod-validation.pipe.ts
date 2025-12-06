8dbf3672a49ed03ebb4d7151a60570dfdca1a634
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-07 01:09:38 +0200

    deleted log

--- src/pipes/zod-validation.pipe.ts
+++ src/pipes/zod-validation.pipe.ts
@@ -5,7 +5,6 @@ export class ZodValidationPipe<T> implements PipeTransform<T, T> {
   constructor(private schema: ZodType<T>) {}
 
   transform(value: T, metadata: ArgumentMetadata): T {
-    console.log('meta data ' + metadata);
     const parsedValue = this.schema.parse(value);
     console.log('value after parsing ' + parsedValue);
     return parsedValue;
