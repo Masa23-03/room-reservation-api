8ad0a61ba3fce922fcfa372d4c1247e89c1122f9
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-07 03:20:44 +0200

    RoleGuard is done

---
+++ src/guards/role.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'generated/prisma';
+import { Roles } from 'src/decorators/role.decorator';
+import { Request } from 'express';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext) {
+    const roles = this.reflector.getAllAndOverride<Role>(Roles, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!roles) return true;
+    const { user } = context.switchToHttp().getRequest<Request>();
+    if (!user) throw new UnauthorizedException();
+    if (!roles.includes(user.role)) throw new ForbiddenException();
+
+    return true;
+  }
+}
