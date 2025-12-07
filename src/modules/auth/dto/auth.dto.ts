2dbde4b5545851c2c6e560d4703696afe7e5e517
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-07 08:33:07 +0200

    auth dto (register , login , authResponse)

--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -1,6 +1,28 @@
 import { UserResponseDTO } from 'src/modules/user/dto/user.dto';
+import { Role, User } from 'generated/prisma';
 
 export type AuthResponseDto = {
   token: string;
   user: UserResponseDTO;
+};
+
+// - RegisterDto:
+//     - email
+//     - password
+//     - name
+//     - role ..only allowed values: GUEST or OWNER — ADMIN blocked
+export type RegisterRole = Extract<Role, 'OWNER' | 'GUEST'>;
+export type RegisterDto = {
+  name: string;
+  password: string;
+  email: string;
+  role: RegisterRole;
+};
+
+// - LoginDto:
+//     - email
+//     - password
+export type LoginDTO = {
+  email: string;
+  password: string;
 };
