95e995e93e1965c72ab9292feda62fcde0f49bca
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-07 09:22:56 +0200

    user dtos

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -1,5 +1,29 @@
-import { User } from 'generated/prisma';
+import { Role, User } from 'generated/prisma';
 
 export type UserResponseDTO = Omit<User, 'password' | 'id' | 'isDeleted'> & {
   id: string;
+};
+
+// - UpdateUserRoleDto (admin) :
+//     - `role` (GUEST or OWNER or ADMIN)
+export type UpdateUserRoleDto = {
+  role: Role;
+};
+
+// - UserOverviewResponseDto (admin):
+//     - `id`
+//     - `email`
+//     - `name`
+//     - `role`
+export type UserOverviewResponseDto = Pick<
+  UserResponseDTO,
+  'id' | 'email' | 'name' | 'role'
+>;
+
+//CreateUserDTO
+export type CreateUserDTO = {
+  name: string;
+  email: string;
+  password: string;
+  role: Role;
 };
