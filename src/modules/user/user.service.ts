6fdab873fa14e09272f86c582a956e302e95b59d
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-07 10:56:56 +0200

    user service is done

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,26 +1,101 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  CreateUserDTO,
+  UpdateUserRoleDto,
+  UserOverviewResponseDto,
+  UserResponseDTO,
+} from './dto/user.dto';
+import { DatabaseService } from '../database/database.service';
+import { User } from 'generated/prisma';
+import { removeFields } from 'src/utils/object.util';
+import {
+  ApiPaginationResponse,
+  PaginationQueryType,
+} from 'src/types/unifiedType';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prismaService: DatabaseService) {}
+  //   - createUser :service only
+  create(createUserDto: CreateUserDTO) {
+    return this.prismaService.user.create({
+      data: createUserDto,
+    });
+  }
+  // - updateUserRole (Admin)
+  updateUserRole(id: bigint, updateRoleDto: UpdateUserRoleDto) {
+    return this.prismaService.user.update({
+      where: { id },
+      data: { role: updateRoleDto.role },
+    });
+  }
+  // - delete (soft) (Admin)
+  remove(id: bigint) {
+    return this.prismaService.user.update({
+      where: { id },
+      data: { isDeleted: true },
+    });
+  }
+  // - FindAll (Admin)
+  findAll(
+    query: PaginationQueryType,
+  ): Promise<ApiPaginationResponse<UserOverviewResponseDto>> {
+    return this.prismaService.$transaction(async (tx) => {
+      const pagination = this.prismaService.handleQueryPagination(query);
+      const total = await tx.user.count({
+        where: { isDeleted: false },
+      });
+      const users = await tx.user.findMany({
+        where: { isDeleted: false },
+
+        ...removeFields(pagination, ['page']),
+
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          role: true,
+        },
+      });
+      const count = await tx.user.count();
+      return {
+        success: true,
+        data: users.map((u) => ({
+          ...u,
+          id: u.id.toString(),
+        })),
+        meta: this.prismaService.formatPaginationResponse({
+          count: count,
+          page: pagination.page,
+          limit: pagination.take,
+        }),
+      };
+    });
+  }
+  // - FindOne (Admin)
+
+  async findOne(id: bigint): Promise<UserResponseDTO> {
+    const user = await this.prismaService.user.findUnique({
+      where: { id },
+    });
+    if (!user) throw new NotFoundException();
+    return this.mapUserWithoutPasswordAndCastBigInt(user);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  // -  findByEmail
+  findByEmail(email: string) {
+    return this.prismaService.user.findUnique({
+      where: { email: email },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+  //mapping user without password and cast bigint into string
+  private mapUserWithoutPasswordAndCastBigInt(user: User): UserResponseDTO {
+    const userWithoutPassword = removeFields(user, ['password', 'isDeleted']);
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return {
+      ...userWithoutPassword,
+      id: String(userWithoutPassword.id),
+    };
   }
 }
