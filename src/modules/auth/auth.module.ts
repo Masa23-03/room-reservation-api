d0cacfef57c30c0b5293473c3c07e80a3598a582
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-07 09:09:33 +0200

    configure JWT module with ConfigService in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { EnvVariable } from 'src/types/declartionMergin';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: (configService: ConfigService<EnvVariable>) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AuthModule {}
