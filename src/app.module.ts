dd96711fdc645b915d6717508383183122fbbb32
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-07 03:18:21 +0200

    added user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './modules/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, UserModule],
 })
 export class AppModule {}
