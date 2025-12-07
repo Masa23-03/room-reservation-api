6faec092891aaf4fa6fcff719c16df7de78f04e8
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-07 08:44:01 +0200

    auth validation schema is done (register , login)

---
+++ src/modules/auth/schema/auth.schema.ts
@@ -0,0 +1,15 @@
+import z, { email, ZodType } from 'zod';
+import { LoginDTO, RegisterDto } from '../dto/auth.dto';
+
+//register
+export const registerValidationSchema = z.object({
+  name: z.string().min(1).max(100),
+  email: z.email().toLowerCase(),
+  password: z.string().min(6).max(100),
+  role: z.enum(['GUEST', 'OWNER']).default('GUEST'),
+}) satisfies ZodType<RegisterDto>;
+//login
+export const loginValidationSchema = registerValidationSchema.pick({
+  email: true,
+  password: true,
+}) satisfies ZodType<LoginDTO>;
