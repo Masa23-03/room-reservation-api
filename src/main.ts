0ad7c246dc0868518c47e3a42c2768c167dd0345
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-07 02:01:31 +0200

    added global to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { HttpExceptionFilter } from './exceptions/http.exception';
+import { PrismaExceptionFilter } from './exceptions/prisma.exception';
+import { ZodExceptionFilter } from './exceptions/zod.exception';
+import { UncaughtExceptionFilter } from './exceptions/global.exception';
 
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new ResponseInterceptor(),
+  );
+  app.useGlobalFilters(
+    new UncaughtExceptionFilter(),
+    new HttpExceptionFilter(),
+    new ZodExceptionFilter(),
+    new PrismaExceptionFilter(),
+  );
   const portEnv = process.env.PORT;
   const port = portEnv ? Number(portEnv) : 3000;
 
   await app.listen(port);
   console.log(' room reservation system is running on port ' + port);
+  console.log('Node env: ' + process.env.NODE_ENV);
 }
 bootstrap();
