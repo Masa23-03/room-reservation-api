94f97a07fbddb2d6e67f91a5f754e73543295294
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-08 08:56:09 +0200

    room query include : filtering and pagination

---
+++ src/modules/room/types/room.query.ts
@@ -0,0 +1,10 @@
+// - RoomQuery (Filtering + Pagination):
+
+import { PaginationQueryType } from 'src/types/unifiedType';
+
+export type RoomQuery = PaginationQueryType & {
+  minCapacity?: number;
+  maxCapacity?: number;
+  minPrice?: number;
+  maxPrice?: number;
+};
