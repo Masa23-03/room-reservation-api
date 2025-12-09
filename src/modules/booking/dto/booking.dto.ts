1d225d8d9a404dc5d35a715d3f9181cfc4860a11
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-09 08:24:15 +0200

    DTOs for booking ( create , response , overview response , update status

---
+++ src/modules/booking/dto/booking.dto.ts
@@ -0,0 +1,43 @@
+import { Booking } from 'generated/prisma';
+// - CreateBookingDto:
+//     - `checkIn`
+//     - `checkOut`
+//     - `room`
+export type CreateBookingDto = Pick<Booking, 'checkIn' | 'checkOut'> & {
+  roomId: string;
+};
+
+// - UpdateBookingStatusDto:
+//     - admin (to confirm)
+//     - guest (to cancel)
+export type UpdateBookingStatusDto = Pick<Booking, 'status'>;
+
+// - BookingResponseDto:
+//     - `id`
+//     - `checkIn`
+//     - `checkOut`
+//     - `status`
+//     - `createdAt`
+//     - `roomId`
+//     - `guestId`
+export type BookingResponseDto = Omit<
+  Booking,
+  'updatedAt' | 'id' | 'roomId' | 'guestId'
+> & { id: string; guestId: string; roomId: string };
+
+// - BookingOverviewResponseDto (for admin):
+//     - `id`
+//     - `roomId`
+//     - `checkIn`
+//     - `checkOut`
+//     - `status`
+export type BookingOverviewResponseDto = Pick<
+  BookingResponseDto,
+  'id' | 'roomId' | 'checkIn' | 'checkOut' | 'status'
+>;
+
+//TODO for later , optional
+// - BookingQueryDto Optional:
+//     - `startDate?`
+//     - `endDate?`
+//     - `roomId?`
