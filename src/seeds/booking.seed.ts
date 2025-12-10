c8b49ffedcd1cff85e4267a76574064438c2ee31
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-10 08:42:30 +0200

    booking seeding

---
+++ src/seeds/booking.seed.ts
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker';
+import { Booking, BookingStatus } from 'generated/prisma';
+
+export const generateBookingSeed = (roomId: bigint, guestId: bigint) => {
+  const checkIn = faker.date.soon({ days: 50 });
+  const stayPeriod = faker.number.int({ min: 1, max: 10 });
+  const checkOut = new Date(
+    checkIn.getTime(),
+    stayPeriod * 24 * 60 * 60 * 1000,
+  );
+  const booking: Omit<
+    Booking,
+    'id' | 'updatedAt' | 'createdAt' | 'room' | 'guest'
+  > = {
+    roomId: roomId,
+    guestId: guestId,
+    status: BookingStatus.PENDING,
+    checkIn: checkIn,
+    checkOut: checkOut,
+  };
+};
