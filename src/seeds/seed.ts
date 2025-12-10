0ffaa4a99067581a1b457a004e2bb695138135ab
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-10 23:53:07 +0200

    corrected prisma import

--- src/seeds/seed.ts
+++ src/seeds/seed.ts
@@ -1,8 +1,7 @@
 import 'dotenv/config';
 import { faker } from '@faker-js/faker';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Role, RoomStatus } from '@prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import { Role, RoomStatus } from '@prisma/client';
 import { generateUserSeed, getAdminUser, getOwnerUser } from './user.seed';
 import { generateRoomSeed } from './room.seed';
 import { generateBookingSeed } from './booking.seed';
