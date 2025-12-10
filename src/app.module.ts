031b1acf1aba5efe8983d8a40c0436108741239b
Masa Sabobeh <masasabobeh03@gmail.com>
2025-12-10 23:51:42 +0200

    added guards to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { ConfigModule } from '@nestjs/config';
 import { RoomModule } from './modules/room/room.module';
 import { BookingModule } from './modules/booking/booking.module';
 import path from 'path';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/auth.guard';
+import { RoleGuard } from './guards/role.guard';
 
 @Module({
   imports: [
@@ -18,6 +21,16 @@ import path from 'path';
     UserModule,
     RoomModule,
     BookingModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
   ],
 })
 export class AppModule {}
